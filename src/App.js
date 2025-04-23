7d080b040803ff05b4aa2aa2503aa0b0c552296c
enochrx <drreayy@gmail.com>
2025-04-23 04:05:59 +0100

    controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "Headphones", quantity: 3, packed: true },
+];
+
 function App() {
   return (
     <div className="app">
@@ -14,20 +23,65 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [select, setSelect] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?😊</h3>
-    </div>
+      <select
+        name=""
+        id=""
+        value={select}
+        onChange={(e) => setSelect(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list"></div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>👜You have X items on your list, and you already packed X</em>
     </footer>
   );
